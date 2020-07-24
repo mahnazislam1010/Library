d6620397d1deaceb589fdc7324846a5f211b4531
Mahnaz Rafia Islam <mahnazislam1@gmail.com>
2020-07-24 20:56:06 +0600

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<b> Designed by : Software Development Lab Ltd.</b> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                  Powered by <b>Software Development Lab Ltd.</b><br />
+                                Copyright &copy; 2020 Software Development Lab Ltd. All rights reserved.
+                </div>
+
+            </div>
+        </div>
+    </section>
